3f93f06713f314fd44ab3cb8dbdfb6ad08f07f70
m.goepfrich <mirko.goepfrich@gmail.com>
2014-06-04 23:27:06 +0200

    #1 Methode carveBitmap hinzugefügt, um Bild zu zerteilen

--- src/de/schiebepuzzle2/PuzzleActivity.java
+++ src/de/schiebepuzzle2/PuzzleActivity.java
@@ -1,7 +1,13 @@
 package de.schiebepuzzle2;
 
+import java.util.HashMap;
+
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MotionEvent;
@@ -11,6 +17,7 @@ import android.view.View.OnTouchListener;
 import android.widget.Button;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class PuzzleActivity extends Activity implements OnTouchListener, OnClickListener{
 	protected int _clickCounter;
@@ -18,6 +25,9 @@ public class PuzzleActivity extends Activity implements OnTouchListener, OnClick
 	protected TextView _counter = null;
 	protected RelativeLayout _relativeLayoutGame = null;
 	protected Button _backButton = null;
+	
+	protected int puzzleSize;
+	protected Bitmap[][] bitmapSnippets;
 	
 	
 	@Override
@@ -33,6 +43,7 @@ public class PuzzleActivity extends Activity implements OnTouchListener, OnClick
 		
 		_relativeLayoutGame.setOnTouchListener(this);
 		_backButton.setOnClickListener(this);
+		
 	}
 
 	@Override
@@ -64,6 +75,44 @@ public class PuzzleActivity extends Activity implements OnTouchListener, OnClick
 	public void onClick(View v) {
 		// TODO Auto-generated method stub
 		this.finish();
+		
+	}
+	
+	public void carveBitmap(){
+		
+		//  Anzahl der seitlichen Puzzleteile
+		puzzleSize = GlobalConstants.EASY;
+		
+		try {
+			Bitmap bitmapFull = BitmapFactory.decodeResource(getResources(), R.drawable.guitar);
+			
+			// Breite + Höhe des Ausschnitts berechnen
+			int targetWidth  = bitmapFull.getWidth () / puzzleSize;
+		    int targetHeight = bitmapFull.getHeight() / puzzleSize;
+		    
+		    bitmapSnippets = new Bitmap[puzzleSize][puzzleSize];
+		    Bitmap bitmapSnip;
+		    
+		    // Puzzleteile erstellen und in Array speichern
+		    for (int i = 0; i < puzzleSize; i++) {
+		    	int startPixelY = 0;
+		    	
+		    	for (int j = 0; j < puzzleSize; j++){
+		    		int startPixelX = 0;
+		    		bitmapSnip = Bitmap.createBitmap(bitmapFull, startPixelX, startPixelY, targetWidth, targetHeight );
+		    		bitmapSnippets[i][j] = bitmapSnip;
+		    		startPixelX += targetWidth;
+		    	}
+		    	startPixelY += targetHeight;
+		    }
+		    
+		}
+		catch (Exception ex) {
+			String errorMessage = "Exception in Methode QuarterBitmapActivity::onCreate() bei Bitmap-Verarbeitung aufgetreten: " + ex.getMessage();
+			Toast.makeText(this, errorMessage, Toast.LENGTH_LONG).show();
+		}
+		
+		
 		
 	}
 
